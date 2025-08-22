238dc9c55fb11fa483f7b5da00568d1e31cb0791
IngePedro24 <gerencia1.genesis@gmail.com>
2025-08-21 22:21:37 -0500

    Delete emprendedores directory

--- emprendedores/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Emprendedor
-
-class EmprendedorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Emprendedor
-        fields = '__all__'
